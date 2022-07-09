f11e68e0fd2cb7749fb42df58a333e575f5e7290
BharatiKashid <108923457+BharatiKashid@users.noreply.github.com>
2022-07-09 16:13:23 +0530

    helllo check my folder bank
    Baknig operations

---
+++ Discoonected/bankingOP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class bankingOP
+{
+
+    public class Account
+    {
+        public double Balance { get; set; }
+
+        public Account(double amount)
+        {
+            this.Balance = amount;
+
+        }
+        public void Deposite(double amount)
+        {
+            this.Balance += amount;
+        }
+
+        public void withdrea (double amt )
+        {
+            this.Balance -= amt;
+        }
+    }
+
+   
+}

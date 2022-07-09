b560a8e42ceff60bdd952a864631f6abba15275d
BharatiKashid <108923457+BharatiKashid@users.noreply.github.com>
2022-07-09 18:16:54 +0530

    Connected Db
    Add new database connectivity code using connected data access

---
+++ Discoonected/coonectedDAL.cs
@@ -0,0 +1,18 @@
+using System.Data.MySqlClient;
+using System.Data;
+using System;
+using System.Collection.Generic;
+
+namespace DAL
+{
+
+public class DBManager
+
+{
+
+ public static string connectionString = @"server=localhost;user=root;password=root;database=knowitdb";
+ 
+ 
+
+}
+}
